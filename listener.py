1821a3881a6bf7c1e6c90932fa353a75a50e3829
Kolbe Kegel <kolbe@kolbekegel.com>
2019-01-10 23:04:32 +0000

    added some new scripts and tools for visualization

--- listener.py
+++ listener.py
@@ -4,22 +4,36 @@
 import os
 import time
 import json
+import traceback
+import mysql.connector as mariadb
 from botocore.credentials import InstanceMetadataProvider, InstanceMetadataFetcher
-
-
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+
+db = mariadb.connect(user='greenhouse', password='', database='greenhouse')
+cursor = db.cursor()
+ins = "insert into sensorReadings (t, temp, humidity) values (from_unixtime(%(ts)s), %(d)s, %(h)s)"
 
 provider = InstanceMetadataProvider(iam_role_fetcher=InstanceMetadataFetcher(timeout=1000, num_attempts=2))
 creds = provider.load()
 
 awsiot = AWSIoTMQTTClient("BaskGreenhouseListener", useWebsocket=True)
 awsiot.configureEndpoint(os.environ['AWSIOT_ENDPOINT'], 443)
-awsiot.configureCredentials("/usr/local/share/ca-certificates/awsiot.pem")
+#awsiot.configureCredentials("/usr/local/share/ca-certificates/awsiot.pem")
+awsiot.configureCredentials("/usr/share/ca-certificates/extra/AmazonRootCA1.pem")
 awsiot.configureIAMCredentials(creds.access_key, creds.secret_key, creds.token)
 
 def newMessage(client, userdata, message):
-    j = json.loads(message.payload)
-    print "{}: new reading at {} of {}ºF and {}%".format(message.topic, time.ctime(j['ts']), j['d'], j['h'])
+    try:
+        j = json.loads(message.payload)
+        data = {'ts':j['ts'], 'd':j['d'], 'h':j['h']}
+        print "{}: new reading at {} of {}ºF and {}%".format(message.topic, time.ctime(j['ts']), j['d'], j['h'])
+        cursor.execute(ins, data)
+        db.commit()
+    except Exception as e:
+        print "Error handling new sensor message!"
+        traceback.print_exc()
+
 
 awsiot.connect()
 awsiot.subscribe("Greenhouse/Stats", 1, newMessage)
---
+++ poll_datasource.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import os
+import time
+import json
+import datetime
+import urlparse
+import mysql.connector as mariadb
+db = mariadb.connect(user='greenhouse', password='', database='greenhouse')
+cursor = db.cursor()
+
+stmt = "select grp*60 ts, round(avg(temp),1) d, round(avg(humidity),1) h from sensorReadings where grp > %s/60 group by grp"
+
+qs = urlparse.parse_qs(os.environ.get('QUERY_STRING',''))
+
+startTime = time.time() - 30*24*3600;
+if 'startTime' in qs:
+   startTime = qs['startTime'][0]
+
+cursor.execute( stmt, (startTime, ) )
+
+data = []
+
+for row in cursor:
+    data.append([datetime.datetime.fromtimestamp(row[0]), float(row[1]), float(row[2])])
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
+
+print "Content-type: application/json"
+print
+print json.dumps(data, cls=DateTimeEncoder)
